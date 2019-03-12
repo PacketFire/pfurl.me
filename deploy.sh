478f01f55e50dba402d03ae9a39596fc67e9b4c7
Brock Ramsey <brockramz@gmail.com>
2019-03-12 04:12:35 -0400

    Add configs for gke deployment (#8)

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+kubectl --server="$GKE_APISERVER" \
+        --namespace="$GKE_NAMESPACE" \
+        --token="$GKE_BEARER_TOKEN" \
+        --insecure-skip-tls-verify=true \
+        set image packetfire/pfurl pfurl=gcr.io/$GCP_PROJECT_ID/pfurl:$TRAVIS_COMMIT
